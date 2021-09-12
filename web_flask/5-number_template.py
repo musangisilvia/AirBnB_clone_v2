b4550cc943aece9f5845372e219ae5d0b537f22b
musangisilvia <musangisilvia@gmail.com>
2021-09-12 19:42:41 +0000

    add task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask Web application listening on
+    0.0.0.0:5000
+"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+        function triggered by URL /
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        function triggered by URL /hbnb
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_variable(text):
+    """
+        function triggered by URL /c/<text>
+    """
+    txt = text.replace("_", " ")
+    return 'C {}'.format(txt)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_var(text='is cool'):
+    """
+        function triggered by URL /python/(<text>)
+    """
+    text = text.replace("_", " ")
+
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+        function triggered by URL /number/<n>
+    """
+    if type(n) is int:
+        return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_temp(n):
+    """
+        function triggered by URL /number_template/<n>
+    """
+    if type(n) is int:
+        return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    """
+        make web page accessible
+    """
+    app.run(host="0.0.0.0")
