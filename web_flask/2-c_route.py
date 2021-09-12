ea3427ae079712328e3b5b92e4a9c183b57a4a73
musangisilvia <musangisilvia@gmail.com>
2021-09-12 18:17:33 +0000

    add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask Web application listening on
+    0.0.0.0:5000
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+        function triggered by URL /
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        function triggered by URL /hbnb
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_variable(text):
+    """
+        function triggered by URL /c/<text>
+    """
+    txt = text.replace("_", " ")
+    return 'C {}'.format(txt)
+
+if __name__ == '__main__':
+    """
+        make web page accessible
+    """
+    app.run(host="0.0.0.0")
