3764fd4122b44ae825085eed468c6d421bf5f33f
musangisilvia <musangisilvia@gmail.com>
2021-09-13 09:15:46 +0000

    add task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask Web application listening on
+    0.0.0.0:5000
+"""
+
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    """
+        displays a html page containing states
+    """
+    return render_template('8-cities_by_states.html',
+                           tes=storage.all("State"))
+
+
+@app.teardown_appcontext
+def teardown(err):
+    storage.close()
+
+
+if __name__ == '__main__':
+    """
+        make web page accessible
+    """
+    app.run(host="0.0.0.0")
